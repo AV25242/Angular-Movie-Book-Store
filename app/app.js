51ae27c88cedb9d4ae77ad9addf0f5c48b43cdca
grastogi <garima211@hotmail.com>
2016-03-10 14:27:16 -0600

    integrated book page

--- app/app.js
+++ app/app.js
@@ -8,47 +8,95 @@ myapp.config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider
 	
 		// for home page and nested view
-		.state('home', {
-			url: '/home',
+		.state('home',  {
+            url: '/home',
 			templateUrl: 'templates/home.html',
 			controller:'MainController'
 			})
-			
-		// for movies page and multiple named views
+        
+      // for movies page and multiple named views
 		.state('movies', {
-			url: '/Movies',
+			url: '/Movies', 
 			templateUrl: 'templates/movies.html',
 			controller: 'MoviesController'			
 			})
-			
-		// for books page and multiple names=d views
+    
+    
+    // for books page and multiple names=d views
 		.state('books', {
 			url: '/Books',
-			templateUrl: 'templates/books.html'
-			
+			templateUrl: 'book_index.html'
+			})
+    
+    // for more books pages
+        
+        .state('book', {
+            abstract: true,
+            url: '/book',
+            templateUrl: 'book.html',
+            controller: 'booksCtrl',
+            onEnter: function(){
+              console.log("enter books");
+            }
+    		
+        })
+        .state('book.list', {
+            url: '/list',
+            // loaded into ui-view of parent's template
+            templateUrl: 'book.list.html',
+            onEnter: function(){
+              console.log("enter books.list");
+            }
+        })
+    
+        .state('book.detail', {
+            url: '/:sku',
+            // loaded into ui-view of parent's template
+            templateUrl: 'book.detail.html',
+            controller: function($scope, $stateParams){
+              $scope.person = $scope.books[$stateParams.sku];
+            },
+            onEnter: function(){
+              console.log("enter books.detail");
+            }
+        })
+    
+		// for login page 
+		.state('SignIn', {
+			url: '/SignIn',
+			templateUrl: 'templates/login.html'
 			})
 });
 
 // controller for home page
 myapp.controller('MainController', function ($scope) {
   $scope.myInterval = 1000;
-  var slides = $scope.slides = [];
-  $scope.addSlide = function() {
-    var newWidth = 1000 + slides.length + 1;
-    slides.push({
-      image: 'http://placekitten.com/' + newWidth + '/300',
-      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-  };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
+  $scope.slides = [
+      {
+          image: './images/BatmanBadBlood.jpg'
+      },
+      {
+          image: './images/Everest.jpg'
+      },
+      {
+          image: './images/Room.jpg'
+      },
+      {
+          image: './images/Minions.jpg'
+      }
+  ];
 });
 
 // controller for movies list 
 myapp.controller('MoviesController', function($scope, $http) {
-	$http.get("moviesCollection.json").then(function (response) {
-		$scope.myData = response.data.moviesRecords;
+	$http.get("./app/moviesCollection.json").then(function (response) {
+		$scope.myMovies = response.data.moviesRecords;
 	});
+});
+
+// controller for books list
+ myapp.controller('booksCtrl', function($scope, $http) {
+  $http.get("json/books.json").then(function (response) {
+      $scope.books = response.data.records;
+  });
 });
